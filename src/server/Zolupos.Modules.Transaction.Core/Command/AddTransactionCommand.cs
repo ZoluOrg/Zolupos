7d6f9143f5e1a49814f14196edc567551538ea6b
Timothy Ganoza <jostimian@gmail.com>
2022-01-22 21:56:09 +0800

    Added AddTransactionCommand And AddTransactionHandler

---
+++ src/server/Zolupos.Modules.Transaction.Core/Command/AddTransactionCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zolupos.Modules.Transaction.Core.Entity;
+
+namespace Zolupos.Modules.Transaction.Core.Command
+{
+    public record AddTransactionCommand(UserTransaction transaction) : IRequest<int>;
+}
---
+++ src/server/Zolupos.Modules.Transaction.Core/Handler/AddTransactionHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Zolupos.Modules.Transaction.Core.Entity;
+using Zolupos.Modules.Transaction.Core.Command;
+using Zolupos.Modules.Transaction.Core.Interface;
+
+namespace Zolupos.Modules.Transaction.Core.Handler
+{
+    public class AddTransactionHandler : IRequestHandler<AddTransactionCommand, int>
+    {
+        private readonly ITransactionDbContext _context;
+        public AddTransactionHandler(ITransactionDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> Handle(AddTransactionCommand request, CancellationToken cancellationToken)
+        {
+            var Transaction = request.transaction;
+            _context.UserTransactions.Add(Transaction);
+            _context.SaveChanges();
+
+            return Transaction.TransactionId;
+        }
+    }
+}
