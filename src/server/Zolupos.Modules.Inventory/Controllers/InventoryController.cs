798c5233e65f19606d40ccec0545fbeccdd1990b
Timothy Ganoza <jostimian@gmail.com>
2022-01-28 22:06:42 +0800

    GetAllTransaction Added

---
+++ src/server/Zolupos.Modules.Inventory/Controllers/InventoryController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zolupos.Modules.Inventory.Core.DTO;
+using Zolupos.Modules.Inventory.Core.Queries;
+
+namespace Zolupos.Modules.Inventory.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : Controller
+    {
+        public readonly IMediator _mediator;
+        public InventoryController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IList<ProductDTO>>> GetAllProduct()
+        {
+            var Products =await _mediator.Send(new GetAllProductQuery());
+            return Ok(Products);
+        }
+    }
+}
