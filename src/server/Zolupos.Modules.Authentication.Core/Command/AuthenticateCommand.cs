b9ce017c2e2913d6983d1a74d50feef56f673c8e
Timothy Ganoza <jostimian@gmail.com>
2022-02-20 15:17:29 +0800

    Use Result Wrapper For Authentication

--- src/server/Zolupos.Modules.Authentication.Core/Command/AuthenticateCommand.cs
+++ src/server/Zolupos.Modules.Authentication.Core/Command/AuthenticateCommand.cs
@@ -5,8 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Zolupos.Modules.Authentication.Core.Annotation;
+using Zolupos.Shared.Core.Wrapper;
 
 namespace Zolupos.Modules.Authentication.Core.Command
 {
-    public record AuthenticateCommand(AuthenticateRequest authenticateRequest) : IRequest<string>;
+    public record AuthenticateCommand(AuthenticateRequest authenticateRequest) : IRequest<ResultWrapper<string>>;
 }
--- src/server/Zolupos.Modules.Authentication.Core/Handler/AuthenticateHandler.cs
+++ src/server/Zolupos.Modules.Authentication.Core/Handler/AuthenticateHandler.cs
@@ -4,16 +4,18 @@ using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Zolupos.Modules.Authentication.Core.Command;
 using Zolupos.Modules.Authentication.Core.Services;
 using Zolupos.Modules.Employee.Infrastructure.Context;
 using Zolupos.Shared.Core.Model;
+using Zolupos.Shared.Core.Wrapper;
 
 namespace Zolupos.Modules.Authentication.Core.Handler
 {
-    public class AuthenticateHandler : IRequestHandler<AuthenticateCommand, string>
+    public class AuthenticateHandler : IRequestHandler<AuthenticateCommand, ResultWrapper<string>>
     {
         public readonly IEmployeeDbContext _context;
         public readonly Settings _settings;
@@ -24,13 +26,13 @@ namespace Zolupos.Modules.Authentication.Core.Handler
             _settings = settings.Value;
         }
 
-        public async Task<string> Handle(AuthenticateCommand request, CancellationToken cancellationToken)
+        public async Task<ResultWrapper<string>> Handle(AuthenticateCommand request, CancellationToken cancellationToken)
         {
             var authRequest = request.authenticateRequest;
             var employee = await _context.Employees.SingleOrDefaultAsync(e => e.PinHashed == authRequest.Pin && e.FirstName == authRequest.FirstName);
             if (employee == null) return null;
             var token = await AuthService.GenerateToken(employee, _settings);
-            return token;
+            return new ResultWrapper<string>(){Data=token, Code = System.Net.HttpStatusCode.OK, Message = ""};
         }
     }
 }
