a106531e8299d41e2c7e8fb57dc26751eb1b204a
Timothy Ganoza <jostimian@gmail.com>
2022-02-21 20:36:36 +0800

    New Home Screen

--- src/client/modules/home/Home.tsx
+++ src/client/modules/home/Home.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useState } from "react";
+import { Button } from "../../components/UI/Button";
+import { BsArrowRightShort } from "react-icons/bs";
+import { IconContext } from "react-icons";
+import { ModuleButton } from "./ModuleButton";
 
 export const Home = () => {
-	return (
-		<div>Home</div>
-	)
-}
+  return (
+    <div className="h-screen flex flex-col justify-evenly">
+      <div className="Header flex flex-col justify-center items-center">
+        <span className="text-4xl font-bold">Welcome To Zolupos</span>
+        <span className="sub">Pick any module module to get started.</span>
+      </div>
+      <div className="flex items-center justify-center gap-2">
+        <ModuleButton Title="POS" Sub="Start Making Sales" />
+        <ModuleButton Title="Customers" Sub="Manage Customers" />
+        <ModuleButton Title="Office" Sub="Manage Inventory" />
+      </div>
+    </div>
+  );
+};
---
+++ src/client/modules/home/ModuleButton.tsx
@@ -0,0 +1,51 @@
+import { motion, AnimatePresence } from "framer-motion";
+import React, { useState } from "react";
+import { IconContext } from "react-icons";
+import { BsArrowRightShort } from "react-icons/bs";
+
+interface Props {
+  Title: string;
+  Sub: string;
+}
+
+const transitionSettings = { type: "Inertia" };
+
+export const ModuleButton: React.FC<Props> = ({ Title, Sub }) => {
+  const [IsHovering, setIsHovering] = useState(false);
+  return (
+    <motion.div
+      className="w w-56 relative rounded overflow-hidden p-2 border cursor-pointer "
+      onMouseOver={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+    >
+      <div>
+        <div className="header">
+          <span className="text-4xl font-bold">{Title}</span>
+        </div>
+        <div className="sub">
+          <span className="text-sm">{Sub}</span>
+        </div>
+      </div>
+      <AnimatePresence>
+        {IsHovering && (
+          <motion.div
+            initial={{ x: -300 }}
+            animate={{ x: 0 }}
+            exit={{ x: 500 }}
+            transition={transitionSettings}
+            className="absolute h-full w-full top-0 left-0 flex items-center justify-center bg-ocean-light backdrop-filter backdrop-blur-sm"
+          >
+            <div className="flex flex-col gap-0 justify-center items-center">
+              <div className="icon">
+                <IconContext.Provider value={{ color: "white" }}>
+                  <BsArrowRightShort size={40} />
+                </IconContext.Provider>
+              </div>
+              <div className="Sub text-white">Open {Title}</div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </motion.div>
+  );
+};
