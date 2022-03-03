16298caaf7fd1b54653ebb039c77b3913a6d7219
Timothy Ganoza <jostimian@gmail.com>
2022-03-03 22:13:08 +0800

    Fixed Current Time Not Showing Time On Load �

--- src/client/components/UI/CurrentTime.tsx
+++ src/client/components/UI/CurrentTime.tsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import dayjs from 'dayjs';
 
 export const CurrentTime = () => {
-	const [currentTime, setTime] = useState("");
+	const [currentTime, setTime] = useState(dayjs().format("YYYY-MM-DD | HH:mm:ss"));
 	useEffect(() => {
 		const getCurrentTime = setInterval(() => {
 			setTime(dayjs().format("YYYY-MM-DD | HH:mm:ss"));
