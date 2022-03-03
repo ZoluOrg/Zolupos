d575d97409ea57723bf7718c7dd17d90861c62c0
Timothy Ganoza <jostimian@gmail.com>
2022-03-03 22:09:56 +0800

    Home Button Working �

--- src/client/modules/pos/Punched/AddProduct.tsx
+++ src/client/modules/pos/Punched/AddProduct.tsx
@@ -4,7 +4,7 @@ import { Input } from '../../../components/UI/Input'
 
 export const SearchProduct = () => {
 	return (
-		<div className="h-14 border-b px-5 items-center flex gap-1 bg-slate-100">
+		<div className="h-14 border-b px-5 items-center flex gap-1">
 			<Input placeholder="Enter Barcode Here To Add" className="w-full"/>
 			<Button>Add</Button>
 		</div>
--- src/client/modules/pos/Punched/TopBar.tsx
+++ src/client/modules/pos/Punched/TopBar.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import {BiHome} from "react-icons/bi"
-import { Button } from '../../../components/UI/Button'
-import { CurrentTime } from '../../../components/UI/CurrentTime'
+import { useRouter } from "next/router";
+import React from "react";
+import { BiHome } from "react-icons/bi";
+import { Button } from "../../../components/UI/Button";
+import { CurrentTime } from "../../../components/UI/CurrentTime";
 
 export const TopBar = () => {
-	return (
-		<div className="topbar flex flex-row items-center justify-between px-5 border-b h-14 w-full">
-			<Button Color='primary' Icon={<BiHome/>}>Home</Button>
-			<CurrentTime/>
-		</div>
-	)
-}
+  const router = useRouter();
+  return (
+    <div className="topbar flex flex-row items-center justify-between px-5 border-b h-14 w-full">
+      <Button
+        Color="primary"
+        Icon={<BiHome />}
+        onClick={() => router.push("../home")}
+      >
+        Home
+      </Button>
+      <CurrentTime />
+    </div>
+  );
+};
