fcbded3a7080d8c00fc3c77760ea1fd078d95b6b
Timothy Ganoza <jostimian@gmail.com>
2022-03-03 22:19:28 +0800

    Use employee name in welcome text

--- src/client/modules/home/Home.tsx
+++ src/client/modules/home/Home.tsx
@@ -5,13 +5,15 @@ import { BsArrowRightShort } from "react-icons/bs";
 import { IconContext } from "react-icons";
 import { ModuleButton } from "./ModuleButton";
 import { PageTransition } from "../animations/PageTransition";
+import { useCredentialContext } from "../../context/CredentialContext";
 
 export const Home = () => {
+  const credsCtx = useCredentialContext();
   return (
     <PageTransition>
       <div className="h-screen flex flex-col justify-evenly">
         <div className="Header flex flex-col justify-center items-center">
-          <span className="text-4xl font-bold">Welcome To Zolupos</span>
+          <span className="text-4xl font-bold">Welcome {credsCtx.creds?.firstName} {credsCtx.creds?.lastName}</span>
           <span className="sub">Pick any module module to get started.</span>
         </div>
         <div className="flex items-center justify-center gap-2">
