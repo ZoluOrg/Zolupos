af714ed9317b4c4d195ee358608f3b7dbd510e3e
Timothy Ganoza <jostimian@gmail.com>
2022-03-17 13:58:22 +0800

    Added animation to autocomplete

--- src/client/modules/pos/Punched/AutoComplete.tsx
+++ src/client/modules/pos/Punched/AutoComplete.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion, MotionConfig } from "framer-motion";
 import React from "react";
 import { usePosContext } from "../../../context/PosContext";
 import { AutoCompleteButton } from "./AutoCompleteButton";
@@ -5,18 +6,25 @@ import { AutoCompleteButton } from "./AutoCompleteButton";
 export const AutoComplete = () => {
   const ctx = usePosContext();
   return (
-    ctx.isSearching && (
-      <div className="autocomplete w-full border-b-2 border-x-2 border-slate-300 rounded-b  absolute bg-bg-light-base p-1">
-        {ctx.searched.map((pr) => (
-          <AutoCompleteButton
-            productName={pr.productName}
-            productQuantity={pr.productQuantity}
-            productManufacturer={pr.productManufacturer}
-            barCode={pr.barCode}
-            productType={pr.productType}
-          />
-        ))}
-      </div>
-    )
+    <AnimatePresence>
+      {ctx.isSearching && (
+        <motion.div
+          className="autocomplete w-full border-b-2 border-x-2 border-slate-300 rounded-b  absolute bg-bg-light-base p-1"
+          initial={{ y: -10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y:-10, opacity: 0 }}
+        >
+          {ctx.searched.map((pr) => (
+            <AutoCompleteButton
+              productName={pr.productName}
+              productQuantity={pr.productQuantity}
+              productManufacturer={pr.productManufacturer}
+              barCode={pr.barCode}
+              productType={pr.productType}
+            />
+          ))}
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
