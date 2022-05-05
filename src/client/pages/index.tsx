dd288dde4bbe3d78974f0255ed46f8b162a498c1
Timothy Ganoza <jostimian@gmail.com>
2022-05-05 14:52:39 +0800

    [FD] Redirect based on jwt

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -1,20 +1,28 @@
-import type { NextPage } from "next";
+import type { GetServerSidePropsContext, NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { Button } from "../components/Button";
 import { UseEmployeeCredentialsContext } from "../context/EmployeeCredentialsContext";
+import { ParseJWT } from "../utils/ParseJWT";
 
 const Home: NextPage = () => {
   const router = useRouter();
-  const Emp = UseEmployeeCredentialsContext();
-  useEffect(() => {
-    if (!Emp.IsLoggedIn) {
-      router.push("/emp/login");
-    }
-  }, []);
-  return <div className=""></div>;
+  return <div className="">Redirecting...</div>;
 };
 
 export default Home;
+
+export const getServerSideProps = (context: GetServerSidePropsContext) => {
+  const Token = context.req.cookies["zolupos-employee-token"];
+  if (Token) {
+    const Parsed = ParseJWT(Token);
+    if (Parsed["exp"] * 1000 <= Date.now()) {
+      return { redirect: { destination: "/emp/login" } };
+    }
+    return { redirect: { destination: "/app/landing" } };
+  } else {
+    return { redirect: { destination: "/emp/login" } };
+  }
+};
