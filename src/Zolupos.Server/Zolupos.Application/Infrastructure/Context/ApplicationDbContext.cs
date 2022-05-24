608e03893d1fe19081e59b9e11e3d7b1f779f923
Timothy Ganoza <jostimian@gmail.com>
2022-05-24 13:10:49 +0800

    Add seeding

--- src/Zolupos.Server/Zolupos.Application/Infrastructure/Context/ApplicationDbContext.cs
+++ src/Zolupos.Server/Zolupos.Application/Infrastructure/Context/ApplicationDbContext.cs
@@ -17,6 +17,12 @@ namespace Zolupos.Application.Infrastructure.Context
         public DbSet<OrderedProduct> OrderedProducts { get; set; }
         public DbSet<Product> Products { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().HasData(new Customer { CustomerId = 1, CustomerName = "Sample Customer", CustomerPoint = 0 });
+            modelBuilder.Entity<Product>().HasData(new Product { ProductId = 1, ProductBarcode = "00001", ProductName = "Sample Product", ProductPrice = 10, ProductQuantity = 10 });
+        }
+
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
         {
             return base.SaveChangesAsync(cancellationToken);
