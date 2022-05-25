489707d801180dbc553243341c2b9ae7272e9660
Timothy Ganoza <jostimian@gmail.com>
2022-05-25 20:50:46 +0800

    [FD] Delete Customer Command Added

---
+++ src/Zolupos.Server/Zolupos.Application/Features/Customers/DeleteCustomer.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zolupos.Application.Common.Interface;
+
+namespace Zolupos.Application.Features.Customers
+{
+    public record DeleteCustomerCommand (int Id) : IRequest<int>;
+    public class DeleteCustomerHandler : IRequestHandler<DeleteCustomerCommand, int>
+    {
+        private IApplicationDbContext _context;
+        public DeleteCustomerHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(DeleteCustomerCommand request, CancellationToken cancellationToken)
+        {
+            var customerToDelete = await _context.Customers.Where(customer => customer.CustomerId == request.Id).FirstAsync();
+            _context.Customers.Remove(customerToDelete);
+            await _context.SaveChangesAsync();
+            return customerToDelete.CustomerId;
+        }
+    }
+}
