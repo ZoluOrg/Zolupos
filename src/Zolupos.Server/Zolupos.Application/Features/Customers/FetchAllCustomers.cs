1a11f3c718f3980e658dd894dd7b61484a2fb6f2
Timothy Ganoza <jostimian@gmail.com>
2022-05-25 20:43:40 +0800

    [FD] Fetch All Customers Query Added

---
+++ src/Zolupos.Server/Zolupos.Application/Features/Customers/FetchAllCustomers.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zolupos.Application.Common.Interface;
+using Zolupos.Application.Entities;
+
+namespace Zolupos.Application.Features.Customers
+{
+    public record FetchAllCustomersQuery : IRequest<ICollection<Customer>>;
+    public class FetchAllCustomersHandler : IRequestHandler<FetchAllCustomersQuery, ICollection<Customer>>
+    {
+        private IApplicationDbContext _context;
+        private IMapper _mapper;
+        public FetchAllCustomersHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<ICollection<Customer>> Handle(FetchAllCustomersQuery request, CancellationToken cancellationToken)
+        {
+            var customers = await _context.Customers.ToListAsync();
+            return customers;
+        }
+    }
+}
