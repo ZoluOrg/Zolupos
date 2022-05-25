77ca1b3f60a0ddb2d213df9b35290c0b4e55cc85
Timothy Ganoza <jostimian@gmail.com>
2022-05-25 21:02:38 +0800

    [FD] Added Customer Controller

---
+++ src/Zolupos.Server/Zolupos.Server/Controllers/Customers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Zolupos.Application.Features.Customers;
+
+namespace Zolupos.Server.Controllers.Customers
+{
+    public class CustomerController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> FetchAllCustomers ()
+        {
+            var customers = await Mediator.Send(new FetchAllCustomersQuery());
+            return Ok(customers);
+        }
+
+        [HttpGet("{Id:int}")]
+        public async Task<ActionResult> FetchCustomerById(int Id)
+        {
+            var customer = await Mediator.Send(new FetchCustomerByIdQuery(Id));
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddProduct(AddCustomerCommand command)
+        {
+            var customer = await Mediator.Send(command);
+            return Ok(customer);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteProduct(DeleteCustomerCommand command)
+        {
+            var customer = await Mediator.Send(command);
+            return Ok(customer);
+        }
+    }
+}
