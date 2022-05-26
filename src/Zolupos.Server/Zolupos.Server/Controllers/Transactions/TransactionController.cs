25442805849d44752476be95966edea5ed38b569
Timothy Ganoza <jostimian@gmail.com>
2022-05-26 15:09:03 +0800

    Transaction Controller

---
+++ src/Zolupos.Server/Zolupos.Server/Controllers/Transactions/TransactionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Zolupos.Application.Features.Transactions;
+
+namespace Zolupos.Server.Controllers.Transactions
+{
+    public class TransactionController : ApiControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> AddTransaction(AddTransactionCommand command)
+        {
+            var transaction = await Mediator.Send(command);
+            return Ok(transaction);
+        }
+    }
+}
