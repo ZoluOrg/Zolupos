36d4c10c92f691254dddea9bc3555141e30293f7
Timothy Ganoza <jostimian@gmail.com>
2022-06-03 21:07:52 +0800

    Add Delete Employee

---
+++ src/Zolupos.Server/Zolupos.Application/Features/Employees/DeleteEmployee.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zolupos.Application.Common.Interface;
+using Zolupos.Application.Common.Wrapper;
+using Zolupos.Application.Entities;
+
+namespace Zolupos.Application.Features.Employees
+{
+    public record DeleteEmployeeCommand (int Id) : IRequest<ResultWrapper<Employee>>;
+    public class DeleteEmployeeHandler : IRequestHandler<DeleteEmployeeCommand, ResultWrapper<Employee>>
+    {
+        private IApplicationDbContext _context;
+        public DeleteEmployeeHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultWrapper<Employee>> Handle(DeleteEmployeeCommand request, CancellationToken cancellationToken)
+        {
+            var employeeToDelete = await _context.Employees.Where(employee => employee.EmployeeId == request.Id).FirstAsync();
+            _context.Employees.Remove(employeeToDelete);
+            await _context.SaveChangesAsync();
+
+            return new ResultWrapper<Employee> { Receive = employeeToDelete, Message = ""};
+        }
+    }
+}
