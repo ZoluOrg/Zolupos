65b27fb9e61c42845b48390ebacff18e6af3f4d2
Timothy Ganoza <jostimian@gmail.com>
2022-08-06 15:10:45 +0800

    UI Changes

--- src/zolupos-pos/src/modules/app/Sales/TransactionCard.tsx
+++ src/zolupos-pos/src/modules/app/Sales/TransactionCard.tsx
@@ -7,7 +7,7 @@ export const TransactionCard: React.FC<{ transaction: ITransaction }> = ({
   transaction,
 }) => {
   return (
-    <div className="grid grid-cols-5 px-5 py-4 bg-mallow-bg-1">
+    <div className="grid grid-cols-5 px-5 py-[19px] bg-mallow-bg-1 items-center">
       <span>{transaction.transactionId}</span>
       <span>{transaction.reference}</span>
       <span>{transaction.total}</span>
