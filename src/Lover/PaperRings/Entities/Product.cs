292d71efa5dd7ee038f12bf81d05cf4aec21143b
!!Timothy <jostimian@gmail.com>
2022-08-27 21:56:32 +0800

    Product Added

---
+++ src/Lover/PaperRings/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperRings.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Barcode { get; set; }
+        public double Price { get; set; }
+        public double UnitCost { get; set; }
+        public bool Vat { get; set; }
+
+        public virtual ICollection<OrderedProduct> OrderedProducts { get; set; }
+    }
+}
