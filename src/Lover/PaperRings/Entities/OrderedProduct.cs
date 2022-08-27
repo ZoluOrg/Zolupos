906a264c36fefd733d9063336d989891b598e7f7
!!Timothy <jostimian@gmail.com>
2022-08-27 21:56:21 +0800

    Ordered Product Added

---
+++ src/Lover/PaperRings/Entities/OrderedProduct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperRings.Entities
+{
+    public class OrderedProduct
+    {
+        public int OrderedProductId { get; set; }
+        public int Quantity { get; set; }
+        public bool BunchPrice { get; set; }
+
+        //Foreign Key
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        //Foreign Key
+        public int TransactionId { get; set; }
+        public Transaction Transaction { get; set; }
+    }
+}
