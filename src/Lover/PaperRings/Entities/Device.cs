f1a4eacfbfd9026d9c3ffecd443122fb427a3391
!!Timothy <jostimian@gmail.com>
2022-08-27 21:55:54 +0800

    Device Added

---
+++ src/Lover/PaperRings/Entities/Device.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperRings.Entities
+{
+    public class Device
+    {
+        public int DeviceId { get; set; }
+        public string DeviceName { get; set; }
+        public DateTime LastRegistration { get; set; }
+        public virtual ICollection<Transaction> Transactions { get; set; }
+    }
+}
